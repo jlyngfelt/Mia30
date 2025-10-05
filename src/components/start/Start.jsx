6229300553a54e76a9d09d2bd3c9df42dd20da31
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-10-05 13:36:05 +0200

    changed way to import gif

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -1,11 +1,12 @@
 import './Start.css';
 import { MainButton } from '../buttons/MainButton';
-import partyImage from '../../../public/images/mia-9x16-gif3-3.3.gif'
+
 
 export const Start = ({onPageChange}) => {
   return (
     <div className="start-container">
-    <img src={partyImage} alt="Fest" className="party-image" />
+
+    <img src="/images/mia-9x16-gif3-3.3.gif" alt="Party" className="party-image"/>
 
       <div className='buttonContainer'>
       <MainButton pageId="information" onPageChange={onPageChange}>
