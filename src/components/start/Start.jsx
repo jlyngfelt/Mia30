281161c7c93a9e68abb7c1747ff1637ca44db693
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-10-05 15:46:52 +0200

    updated design on startpage

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -5,9 +5,14 @@ import { MainButton } from '../buttons/MainButton';
 export const Start = ({onPageChange}) => {
   return (
     <div className="start-container">
-
-    <img src="/images/mia-9x16-gif3-3.3.gif" alt="Party" className="party-image"/>
-
+      <h1>Mia Westh</h1>
+      <h1 className='big'>30</h1>
+      <div className='textbox'>
+      <h4>Hanssons loge 6/6 2026</h4>
+    <img src="/images/mia-9x16-gif3-3.3.gif" alt="Party-gif" className="party-image"/>
+      </div>
+<div className='mountainContainer'>
+<h1 className='big celebrate'>Mia fyller 30 & jävlar vad det ska firas</h1>
       <div className='buttonContainer'>
       <MainButton pageId="information" onPageChange={onPageChange}>
           INFORMATION
@@ -17,6 +22,7 @@ export const Start = ({onPageChange}) => {
           OSA
         </MainButton>
       </div>
+</div>
     </div>
   );
 };
