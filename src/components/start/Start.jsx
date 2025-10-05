df7c8b6eefe8424a3b2d9e2304f0a90ae5f1148d
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-10-05 13:05:47 +0200

    changed gif

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -1,11 +1,11 @@
 import './Start.css';
 import { MainButton } from '../buttons/MainButton';
-import partyImage from '../../../public/images/mia-9x16-low-min.gif'
+import partyImage from '../../../public/images/mia-9x16-gif2-6.1.gif'
 
 export const Start = ({onPageChange}) => {
   return (
     <div className="start-container">
-    {/* <img src={partyImage} alt="Fest" className="party-image" /> */}
+    <img src={partyImage} alt="Fest" className="party-image" />
 
       <div className='buttonContainer'>
       <MainButton pageId="information" onPageChange={onPageChange}>
