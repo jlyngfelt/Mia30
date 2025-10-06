21c48478cdba3b8ccd075471e09996b8e5161cef
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-10-06 21:01:22 +0200

    test for designbug deployed version

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -3,18 +3,19 @@ import { MainButton } from '../buttons/MainButton';
 
 
 export const Start = ({onPageChange}) => {
-  return (
+ return (
     <div className="start-container">
       <h1>Mia Westh</h1>
       <h1 className='big'>30</h1>
       <div className='textbox'>
-      <h4>Hanssons loge 6/6 2026</h4>
-    <img src="/images/mia-9x16-gif3-3.3.gif" alt="Party-gif" className="party-image"/>
+        <h4>Hanssons loge 6/6 2026</h4>
+        <img src="/images/mia-9x16-gif3-3.3.gif" alt="Party-gif" className="party-image"/>
       </div>
-<div className='mountainContainer marginContainer'>
-<h1 className='big celebrate'>Mia fyller 30 & jävlar vad det ska firas</h1>
+      
+      <h1 className='big celebrate'>Mia fyller 30 & jävlar vad det ska firas</h1>
+      
       <div className='buttonContainer'>
-      <MainButton pageId="information" onPageChange={onPageChange}>
+        <MainButton pageId="information" onPageChange={onPageChange}>
           INFORMATION
         </MainButton>
         
@@ -22,7 +23,8 @@ export const Start = ({onPageChange}) => {
           OSA
         </MainButton>
       </div>
-</div>
+      
+      <div className='mountainContainer'></div>
     </div>
   );
 };
